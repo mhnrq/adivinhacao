9a298cc0e8712b55fe44ecc3dc27f174812d8c2f
mhnrq <miguelhrodrigues02@gmail.com>
2025-07-30 11:52:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+body {
+  margin: 0;
+  font-family: sans-serif;
+}
 
+#jogo {
+  position: relative;
+  width: 768px;
+  height: 768px;
+  margin: auto;
+}
+
+#fundo {
+  width: 100%;
+  height: 100%;
+}
+
+#fala {
+  position: absolute;
+  top: 60px;
+  left: 230px;
+  width: 460px;
+  font-size: 20px;
+  line-height: 1.5;
+}
+
+button {
+  position: absolute;
+  width: 180px;
+  height: 50px;
+  background: transparent;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+}
+
+#btn1 {
+  top: 460px;
+  left: 490px;
+}
+
+#btn2 {
+  top: 460px;
+  left: 490px;
+  display: none;
+}
+
+#btn3 {
+  top: 525px;
+  left: 490px;
+  display: none;
+}
